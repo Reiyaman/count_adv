17a3da6718dfee82f2a49685075f9405a1d67386
Reiyaman <Reiyaman>
2021-09-05 16:10:52 +0000

    小テスト レベル２カウンタ増やす

--- app.rb
+++ app.rb
@@ -15,6 +15,8 @@ end
 
 get '/count' do
   @number = Count.first.number
+  Count.create!(number: "number2")
+  @number2 = Count.second.number
   erb :index
 end
 
@@ -36,5 +38,54 @@ post '/clear' do
   count = Count.first
   count.number = 0
   count.save
+  redirect '/count'
+end
+
+post '/kakeru' do
+  count = Count.first
+  count.number = count.number * 2
+  count.save
+  redirect '/count'
+end
+
+post '/waru' do
+  count = Count.first
+  count.number = count.number / 2
+  count.save
+  redirect '/count'
+end
+
+post '/plus2' do
+  count2 = Count.second
+  count2.number = count2.number + 1
+  count2.save
+  redirect '/count'
+end
+
+post '/minus2' do
+  count2 = Count.second
+  count2.number = count2.number - 1
+  count.save
+  redirect '/count'
+end
+
+post '/clear2' do
+  count2 = Count.second
+  count2.number = 0
+  count2.save
+  redirect '/count'
+end
+
+post '/kakeru2' do
+  count2 = Count.second
+  count2.number = count2.number * 2
+  count2.save
+  redirect '/count'
+end
+
+post '/waru2' do
+  count2 = Count.second
+  count2.number = count2.number / 2
+  count2.save
   redirect '/count'
 end
